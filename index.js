be1c3c1f224e105b33c3f9d36a9df03ec6d5a1d5
tuagkcuf <jokersms222@gmail.com>
2023-03-29 14:52:42 +0300

    connect local

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import {ethers} from "./ethers-5.6.esm.min.js"
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
 const fundButton = document.getElementById("fundButton")
@@ -21,6 +22,12 @@ async function connect() {
 async function fund(ethAmount) {
     console.log(`Funding with ${ethAmount}`)
     if (typeof window.ethereum !== "undefined") {
-
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.contractAddress(
+            contractAddress,
+            abi,
+            signer
+        )
     }
 }
