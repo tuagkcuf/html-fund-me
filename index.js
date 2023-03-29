cb9c1f9c6e2aad01167247a2e273488cfa70f3f8
tuagkcuf <jokersms222@gmail.com>
2023-03-29 14:45:57 +0300

    add ethers

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+import {ethers} from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+console.log(ethers)
+
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
         await window.ethereum.request({
             method: "eth_requestAccounts",
-        });
-        document.getElementById("connect-btn").innerHTML =
-            "Connected";
+        })
+        connectButton.innerHTML = "Connected"
     } else {
-        document.getElementById("connect-btn").innerHTML =
-            "Install metamask";
+        connectButton.innerHTML = "Install metamask"
     }
 }
 
 async function fund(ethAmount) {
-    console.log(first)
+    console.log(`Funding with ${ethAmount}`)
+    if (typeof window.ethereum !== "undefined") {
+
+    }
 }
